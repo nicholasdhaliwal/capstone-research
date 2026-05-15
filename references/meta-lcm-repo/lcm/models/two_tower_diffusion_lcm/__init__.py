72b7f1512a955bff32d2ef5bb8598cba423f9580
Nick Dhaliwal <98420849+nicholasdhaliwal@users.noreply.github.com>
2026-05-15 11:21:34 -0500

    fix gitignore after folder rename

--- references/meta-lcm-repo/lcm/models/two_tower_diffusion_lcm/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-#
-
-# Register architectures
-import lcm.models.two_tower_diffusion_lcm.archs  # noqa
--- references/meta-lcm-repo/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-#
-
-import pytest
-
-pytest.register_assert_rewrite("tests.common")
